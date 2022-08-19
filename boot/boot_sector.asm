f88cdb83b148b3e1fb7e6f1c6bba5e81a25defd4
Micah Edwards <m1cah@hotmail.co.uk>
2022-08-19 10:31:09 +0100

    Add labels to asm code. consider mem addresses

--- boot/boot_sector.asm
+++ boot/boot_sector.asm
@@ -1,18 +1,25 @@
-; with x86 arch, we have an ax register for IO and arithmetic. we will be using it for screen IO.
-; the ah (higher) will be used for function and al (lower 8 bits) for data.
 
-mov ah, 0x0e ; tell video interrupt what you want to do. we want to draw contents of al  using tty mode. 
-mov al, "H" ; lower side of ax register will hold chars to draw on screen
-int 0x10 ; raise interrupt to draw "H" on screen
-mov al, "E"
+; tell assmebler preprocessor to automatically set the base address for labels to start at 0x7c00 (start of bootsector) and not 0x0000. Why? Boot sector code starts at 0x7c00
+
+[org 0x7c00]
+
+mov ah, 0x0e 
+mov al, "M"
+int 0x10 
+mov al, "I"
 int 0x10
-mov al, "L"
+mov al, "C"
 int 0x10
+mov al, "A"
 int 0x10
-mov al, "O"
+mov al, [some_char] ; access value held by pointer (indirect addressing)
 int 0x10
+
 
 jmp $
+
+some_char:
+	db "H" ; if we were to access this address, we would get just its offset (25 bytes). 25 bytes in main memory would point to somewhere in the IVT. So we would need to either use a preprocessor directive to adjust the base address automatically or manually account for where this code lives in main memory every time.
 
 times 510-($-$$) db 0	; pad out machine instructions with 0s from 
 
