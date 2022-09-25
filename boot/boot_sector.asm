5b916238bbdb619ee3a7480f2df8a7bb489aec4d
Micah <m1cah@hotmail.co.uk>
2022-09-25 15:55:26 +0100

    allow boot_sector code to copy from hdd to memory

--- boot/boot_sector.asm
+++ boot/boot_sector.asm
@@ -1,35 +1,14 @@
-; The aim is to now load the kernel into memory, as it wont fit in our boot sector. This is why the boot sector is known to be bootstrap code for the kernel. 
-; To access the kernel, which resides on the disk, we need to know how to read using disk drives. 
-; The BIOS provides a useful abstraction for data drives (disk, floppy) and we will use them to read. 
+; NOTE: you need to set -fda flag for qemu to work with hard drive booting 
+; so qemu-system-x86 -fda boot_sector.bin 
 
-; Imagine a platter with a head (like a vinyl player) but several of them stacked together. Thats our hard drive.
-; How do we access a specific block of data within stacked platters?
+[org 0x7c00]
 
-; We can use a 3D esque coordinate system - Cylinder Head Sector (addressing):
- 
-; Cylinder - Imagine a cylinder clipping through all of the platters. You will then notice a circle that the 
-; cylinder forms is a track for all platters. So a cyclinder is the set of tracks for all cylinders.
+; we aim to load 5 sectors to 0x0000(ES):0x9000(BX)
 
-; Head - this allows us to select an individual platter, as there is usually a 2-1 association of head to platter
-; one is head for reading and one for writing.
+mov [BOOT_DRIVE], dl 	; BIOS stores our boot drive in dl, so we remember it for later
 
-; Sector - A track is divided by sectors, usually in 512B. So we can select which one here.
-
-; To use the BIOS' interface for accessing the drives, it expects data to be placed in certain registers. 
-
-; https://www.youtube.com/watch?v=d0xn68w3KPE for a visualisation of the disk
-
-mov ah, 0x02	; BIOS read sector function
-
-
-; CHS addressing scheme 
-mov dl, 0	; Read drive 0
-mov ch, 3	; Select cylinder 3
-mov dh, 1	; Select track 2 - these are 0 indexed
-mov cl, 4	; select 4th sector on the track - these are 1 indexed
-
-mov al, 5	; read 5 sectors from the start point (4th sector - specified above)
-
+mov bp, 0x8000	; set sp and bp out of the way
+mov sp, bp	; way at 0x8000 
 
 ; we now want to tell the BIOS where to copy the data in memory. For this we use segment registers
 ; (to again allow us to access more regions of memory in real mode). 
@@ -37,22 +16,34 @@ mov al, 5	; read 5 sectors from the start point (4th sector - specified above)
 ; The BIOS expects us to use the extra segment register (es) and bx as our "offset from base offset". 
 ; this is ES:BX
 
-mov bx, 0xa000
-mov es, bx
-mov bx, 0x1234
+mov bx, 0x9000	; set 0x9000 as offset from es segment register. this will be the start addr for in-memory hdd data.
+mov dl, [BOOT_DRIVE]
+mov dh, [SECTORS_TO_READ]
+call disk_load
 
-; remember, cpu will do (0xa000 * 16 + 0x1234) -> 0xa1234
-int 0x13 ; issue BIOS to perform the read from disk.
+mov dx, [0x9000]	; print first loaded word
+call print_hex
 
-
+mov dx, [0x9000 + 512]	; print first word from 2nd loaded sector
+call print_hex
 
 jmp $
 
-some_char:
-db "A"
+%include "print_hex.asm"
+%include "disk_load.asm"
+%include "print_string.asm"
 
+BOOT_DRIVE:
+db 0
 
-times 510-($-$$) db 0
+SECTORS_TO_READ:
+db 5
+
+times 510-($-$$) db 0 ; db means define byte - in this create 0 bytes 
 dw 0xaa55
+
+; add a sanity check. we will try to print these when we read from disk
+times 256 dw 0xdada ; dw means create word- in this case, define 256 words of 0xdada (16-bit each because of protectedmode)
+times 256 dw 0xface ; 256*2 = 512 bytes of memory reserved.
 
 
--- boot/disk_load.asm
+++ boot/disk_load.asm
@@ -21,28 +21,18 @@
 
 
 disk_load:
+	push dx ; dx (data register) is what we used to pass an argument concerning how many sectors to read
+	; but we will also need to use it in order to perform a read operation via the BIOS interface.
+	; so we will push it to the stack to save it for later use.	
 
 	mov ah, 0x02	; BIOS read sector function
-
+		
+	mov al, dh ; read dh amount of sectors from start sector
 
 	; CHS addressing scheme 
-	mov dl, 0	; Read drive 0
-	mov ch, 3	; Select cylinder 3
-	mov dh, 1	; Select track 2 - these are 0 indexed
-	mov cl, 4	; select 4th sector on the track - these are 1 indexed
-
-	mov al, SECTOR_READ_AMOUNT 	; read 5 sectors from the start point (4th sector - specified above)
-
-
-	; we now want to tell the BIOS where to copy the data in memory. For this we use segment registers
-	; (to again allow us to access more regions of memory in real mode). 
-
-	; The BIOS expects us to use the extra segment register (es) and bx as our "offset from base offset". 
-	; this is ES:BX
-
-	mov bx, 0xa000
-	mov es, bx
-	mov bx, 0x1234
+	mov ch, 0x00	; Select cylinder 0
+	mov dh, 0x00	; Select head 0 - these are 0 indexed
+	mov cl, 0x02	; select 2nd sector on the track - these are 1 indexed
 
 	; remember, cpu will do (0xa000 * 16 + 0x1234) -> 0xa1234
 	int 0x13 ; issue BIOS to perform the read from disk.
@@ -51,11 +41,25 @@ disk_load:
 	; here we perform error checking. BIOS updates registers to signal faults occured.
 	
 	; carry flag (CF) is set to signal a fault 
-	jc disk_error ; jump if carry flag was set
-	
+	jc disk_carry_flag_set_err ; jump if carry flag was set
+		
+
+	pop dx;
+
 	; BIOS will set al as the number of sectors successfully read.
-	cmp al, SECTOR_READ_AMOUNT ; remember we requested to read 5 sectors from the start point? how many of those sector reads were successful
-	jne disk_error
+	cmp al, dh ; remember we requested to read 5 sectors from the start point? how many of those sector reads were successful
+	jne disk_sector_read_failure
+	ret
+
+disk_carry_flag_set_err:
+	mov bx, DISK_CARRY_FLAG_SET_ERR_MSG
+	call print_string
+	jmp disk_error
+
+disk_sector_read_failure:
+	mov bx, DISK_SECTOR_READ_ERR_MSG
+ 	call print_string
+	jmp disk_error
 
 disk_error:
 	mov bx, DISK_ERROR_MSG
@@ -63,4 +67,5 @@ disk_error:
 	jmp $
 
 DISK_ERROR_MSG: db "Disk read error!", 0
-SECTOR_READ_AMOUNT
+DISK_CARRY_FLAG_SET_ERR_MSG: db "Carry flag was set ", 0
+DISK_SECTOR_READ_ERR_MSG: db "Failed to read one or more sectors", 0
