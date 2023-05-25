444de58cfaf8049076f568760dd58ab4d4b80e0f
Micah <m1cah@hotmail.co.uk>
2023-05-25 15:43:40 +0100

    renaming

--- kernel/vmm.h
+++ kernel/vmm.h
@@ -7,14 +7,14 @@
 #include "pmm.h"
 
 
-#define PAGE_DIRECTORY_ADDR 0xa0000000
 #define PAGE_SIZE 4096
 #define PTE_SIZE_BYTES 4
 #define PDE_SIZE_BYTES 4
 #define PT_SIZE_BYTES PAGE_SIZE * MAX_PTE_COUNT
 
 #define MAX_PTE_COUNT 1024
-#define MAX_PD_COUNT 1024
+#define MAX_PDE_COUNT 1024
+#define PROCESS_PTE_COUNT MAX_PDE_COUNT*MAX_PTE_COUNT
 // #define PT_BASE_ADDR 0x42000 // 100kb into free memory region of lower memory. 4kb aligned
 // #define PD_BASE_ADDR 0x21000
 
@@ -29,7 +29,6 @@
 #define KERNEL_PD_INDEX 1 
 
 #define VMM_PRESENT 1
-
 
 
 uint32_t create_pde(
@@ -59,7 +58,6 @@ uint32_t create_pte(
 
 void vmm_init(void);
 
-// only here for testing purposes 
 void init_page_directory(void);
 void create_page_table(uint16_t pd_index);
 uint32_t palloc(uint16_t pd_index, int n_allocs);
